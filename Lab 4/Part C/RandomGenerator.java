f36f892764f9767320b04f8b3ac845d741269de0
yideax <yidadou@gmail.com>
2016-02-09 17:45:05 -0700

    This is harrys code

--- Lab 4/Part C/RandomGenerator.java
+++ Lab 4/Part C/RandomGenerator.java
@@ -10,7 +10,7 @@ class RandomGenerator {
  * @param hi
  * @return
  */
-	int discrete(int lo, int hi)
+	int [] discrete(int lo, int hi)
 	{
 		if(lo >= hi){
 			System.out.println("Error discrete, lo >= hi");
@@ -18,7 +18,9 @@ class RandomGenerator {
 		}
 		
 		Random r = new Random();
-		int d = r.nextInt(hi - lo + 1) + lo;
+		int d[] = new int [2];
+		d[0] = r.nextInt(hi - lo + 1) + lo;
+		d[1] = r.nextInt(hi - lo + 1) + lo;
 		return d;
 	}
 	
--- Lab 4/Part C/RandomPlayer.java
+++ Lab 4/Part C/RandomPlayer.java
@@ -1,21 +1,81 @@
-import java.io.*;
-import java.text.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-/**
- * ENSF 409 - Lab 3 - Winter 2016
- * @author  Harry Han, Yida Xu
- * @version 1.0
- * @since February 2, 2015
- */
- 
- 	/**
-	 * A class constructor that inherits Players private members for human players
-	 */ 
-public class RandomPlayer extends Player{
-		public RandomPlayer(){
-		}
+public class RandomPlayer extends HumanPlayer{
 	
-		public RandomPlayer(String name, char mark, Board board) {
-			super(name, mark, board);
-		}
+	public RandomPlayer(){
 	}
+
+	public RandomPlayer(String name, char mark, Board board) {
+		super(name, mark, board);
+	}
+	
+	public void makeMove(){
+		
+		int col, row;
+		RandomGenerator x = new RandomGenerator();
+		
+		int[] picker = new int [2];
+		picker = x.discrete(0,2);
+		
+		col = picker[0];
+		row = picker[1];
+		
+		if (board.getMark(row, col) == 'O' || board.getMark(row, col) == 'X'){
+			makeMove();
+		}
+		else {
+			board.addMark(row, col, mark);
+			board.display();
+		}
+		
+		
+	}
+
+
+	public void play() throws IOException {
+		Integer move = -1;
+		
+		while(true) {
+			BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+			System.out.print("Who will play first?\nEnter '0' for Player 1 (X) or '1' for Player 2 (O): ");
+			String input = stdin.readLine().trim();
+
+			while (input.isEmpty() || parseInt(input) == null) {
+				System.out.println("\nPlease try again.");
+				System.out.print("Who will play first?\nEnter '0' for Player 1 (X) or '1' for Player 2 (O): ");
+				input = stdin.readLine();
+			}
+
+			move = parseInt(input);
+			if (move == 1 || move == 0) {
+				break;
+				}
+			}
+
+		while(board.xWins() != 1 && board.oWins() != 1 && board.isFull() !=true) {
+			if(move == 0){
+				makeMove();
+				move = 1;
+			}
+			else {
+				opponent.makeMove();
+				move = 0;
+			}
+		}
+		
+		if(board.xWins() == 1 || board.oWins() == 1) {
+			System.out.print("\nThe winner of the game is: ");
+			if(board.checkWinner(mark) == 1){
+				System.out.println(name + "\n");
+			}
+			else
+				System.out.println(opponent.name + "\n");
+		}
+		else
+			System.out.println("\nThe game resulted in a tie.\n");
+	}
+	
+
+}
